a21e30300bc48268097dd0f1d9742e9f91b6ed0a
Sheeraz Ali <sheerazali656@gmail.com>
2020-06-30 01:23:22 -0700

    Initial Commit

---
+++ Crud_With_ListBox/Crud_With_ListBox/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Crud_With_ListBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBoxSports = new System.Windows.Forms.ListBox();
+            this.textBoxItems = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonInsertItem = new System.Windows.Forms.Button();
+            this.buttonUpdateItem = new System.Windows.Forms.Button();
+            this.buttonDeleteItem = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(92, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CRUD WITH LISTBOX";
+            // 
+            // listBoxSports
+            // 
+            this.listBoxSports.FormattingEnabled = true;
+            this.listBoxSports.Location = new System.Drawing.Point(24, 66);
+            this.listBoxSports.Name = "listBoxSports";
+            this.listBoxSports.Size = new System.Drawing.Size(127, 212);
+            this.listBoxSports.TabIndex = 1;
+            this.listBoxSports.SelectedIndexChanged += new System.EventHandler(this.listBoxSports_SelectedIndexChanged);
+            // 
+            // textBoxItems
+            // 
+            this.textBoxItems.Location = new System.Drawing.Point(172, 92);
+            this.textBoxItems.Name = "textBoxItems";
+            this.textBoxItems.Size = new System.Drawing.Size(163, 20);
+            this.textBoxItems.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(169, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Add Items:";
+            // 
+            // buttonInsertItem
+            // 
+            this.buttonInsertItem.Location = new System.Drawing.Point(200, 135);
+            this.buttonInsertItem.Name = "buttonInsertItem";
+            this.buttonInsertItem.Size = new System.Drawing.Size(102, 32);
+            this.buttonInsertItem.TabIndex = 4;
+            this.buttonInsertItem.Text = "Insert Item";
+            this.buttonInsertItem.UseVisualStyleBackColor = true;
+            this.buttonInsertItem.Click += new System.EventHandler(this.buttonInsertItem_Click);
+            // 
+            // buttonUpdateItem
+            // 
+            this.buttonUpdateItem.Location = new System.Drawing.Point(200, 189);
+            this.buttonUpdateItem.Name = "buttonUpdateItem";
+            this.buttonUpdateItem.Size = new System.Drawing.Size(102, 32);
+            this.buttonUpdateItem.TabIndex = 4;
+            this.buttonUpdateItem.Text = "Update Item";
+            this.buttonUpdateItem.UseVisualStyleBackColor = true;
+            this.buttonUpdateItem.Click += new System.EventHandler(this.buttonUpdateItem_Click);
+            // 
+            // buttonDeleteItem
+            // 
+            this.buttonDeleteItem.Location = new System.Drawing.Point(200, 243);
+            this.buttonDeleteItem.Name = "buttonDeleteItem";
+            this.buttonDeleteItem.Size = new System.Drawing.Size(102, 32);
+            this.buttonDeleteItem.TabIndex = 4;
+            this.buttonDeleteItem.Text = "Delete Item";
+            this.buttonDeleteItem.UseVisualStyleBackColor = true;
+            this.buttonDeleteItem.Click += new System.EventHandler(this.buttonDeleteItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(358, 315);
+            this.Controls.Add(this.buttonDeleteItem);
+            this.Controls.Add(this.buttonUpdateItem);
+            this.Controls.Add(this.buttonInsertItem);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxItems);
+            this.Controls.Add(this.listBoxSports);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBoxSports;
+        private System.Windows.Forms.TextBox textBoxItems;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonInsertItem;
+        private System.Windows.Forms.Button buttonUpdateItem;
+        private System.Windows.Forms.Button buttonDeleteItem;
+    }
+}
+
---
+++ Crud_With_ListBox/Crud_With_ListBox/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Crud_With_ListBox
+{
+    public partial class Form1 : Form
+    {
+        string cs = ConfigurationManager.ConnectionStrings["csdb"].ConnectionString;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadData_In_ListBox();
+        }
+
+        public void LoadData_In_ListBox() {
+
+            SqlConnection conn = new SqlConnection(cs);
+            string query = "select * from Sports";
+            SqlCommand cmd = new SqlCommand(query, conn);
+
+            conn.Open();
+
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read()) {
+
+                string name = reader.GetString(0);
+                listBoxSports.Items.Add(name);
+            }
+
+            conn.Close();
+
+
+        }
+
+        private void buttonInsertItem_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(cs);
+            string query = "insert into Sports values(@items)";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@items", textBoxItems.Text);
+
+            conn.Open();
+
+            int n = cmd.ExecuteNonQuery();
+
+            if (n > 0)
+            {
+
+                MessageBox.Show("Insertion Successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                listBoxSports.Items.Clear();
+                LoadData_In_ListBox();
+            }
+            else {
+
+                MessageBox.Show("Insertion Failed.", "Failure", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            conn.Close();
+            textBoxItems.Clear();
+            textBoxItems.Focus();
+
+
+        }
+
+        private void buttonUpdateItem_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(cs);
+            string query = "update Sports set Name = @name where Name = @items";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@name", textBoxItems.Text);
+            cmd.Parameters.AddWithValue("@items", listBoxSports.SelectedItem);
+
+            conn.Open();
+
+            int n = cmd.ExecuteNonQuery();
+
+            if (n > 0)
+            {
+
+                MessageBox.Show("Updation Successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                listBoxSports.Items.Clear();
+                LoadData_In_ListBox();
+            }
+            else
+            {
+
+                MessageBox.Show("Updation Failed.", "Failure", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            conn.Close();
+            textBoxItems.Clear();
+            textBoxItems.Focus();
+        }
+
+        private void listBoxSports_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBoxItems.Text = listBoxSports.SelectedItem.ToString();
+        }
+
+        private void buttonDeleteItem_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(cs);
+            string query = "delete from Sports where Name = @items";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@items", textBoxItems.Text);
+
+            conn.Open();
+
+            int n = cmd.ExecuteNonQuery();
+
+            if (n > 0)
+            {
+
+                MessageBox.Show("Deletion Successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                listBoxSports.Items.Clear();
+                LoadData_In_ListBox();
+            }
+            else
+            {
+
+                MessageBox.Show("Deletion Failed.", "Failure", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            conn.Close();
+            textBoxItems.Clear();
+            textBoxItems.Focus();
+        }
+    }
+}
